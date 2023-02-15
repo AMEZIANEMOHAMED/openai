7044fabe2a5e9be003b858bb5be85336a70e429a
AMEZIANEMOHAMED <87206808+AMEZIANEMOHAMED@users.noreply.github.com>
2023-02-15 14:02:21 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,14 +7,16 @@ dotenv.config();
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello World'
+        message: 'Hello friends! The API server is up and running. This API was built by Viraj.',
     })
 });
 
