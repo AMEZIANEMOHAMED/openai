2f30a511e87bd95496a7cc6da7bfa0b8d2d37af4
AMEZIANEMOHAMED <87206808+AMEZIANEMOHAMED@users.noreply.github.com>
2023-02-15 14:43:31 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,51 +2,50 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
+
 dotenv.config();
 
+const port = 5000;
+
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-
-const corsOptions ={
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello friends! The API server is up and running. This API was built by me.',
+        message: 'Hello friends! The API server is up and running. This API was built by Viraj.',
     })
 });
 
 app.post('/', async (req, res) => {
     try {
-        const promt = req.body.promt;
+        const prompt = req.body.prompt;
+
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `${promt}`,
+            prompt: `${prompt}`,
             temperature: 0,
             max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
-          })
-          
-          res.status(200).send({
-            bot: response.data.choices[0].text
-          })
+        });
 
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
     } catch (error) {
         console.log(error);
-        res.status(500).send({error});
+        res.status(500).send(error);
     }
-});
+})
 
-app.listen(3000, () => console.log('server is running on port http://localhost:3000'));
+app.listen(port,
+    () => console.log(`Server is running on http://localhost:${port}`)
+);
